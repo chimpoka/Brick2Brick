e1498d2e6c519f55418385ccd3af845cf442a908
chimpoka <artem.voronov90@gmail.com>
2018-06-13 12:53:21 +0300

    New font. Russian v_1.03

---
+++ Puzzle/Assets/Scripts/FontSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FontSwitch : MonoBehaviour
+{
+    public Font font;
+
+    private void Start()
+    {
+        GetComponentInParent<Text>().font = font;
+    }
+}
